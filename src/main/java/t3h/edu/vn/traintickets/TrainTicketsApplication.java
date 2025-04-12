dc528e8229d7f01db7ef317002a0d8e5c3990803
Anhle <laplainao1@gmail.com>
2025-04-12 23:07:34 +0700

    initial commit

---
+++ src/main/java/t3h/edu/vn/traintickets/TrainTicketsApplication.java
@@ -0,0 +1,13 @@
+package t3h.edu.vn.traintickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/t3h/edu/vn/traintickets/TrainTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package t3h.edu.vn.traintickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
